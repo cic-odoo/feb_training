c7a626d8a7ef7819bd84e967a730caa669b89cd2
cic-odoo <cic@odoo.com>
2021-02-03 22:01:24 +0000

    [ADD] views fields model

--- art_gallery/__manifest__.py
+++ art_gallery/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/art_gallery_views.xml',
 
     ],
     # only loaded in demonstration mode
---
+++ art_gallery/models/art_work.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from datetime import date
+
+class ArtWork(models.Model):
+    _name = 'art_gallery.art.work'
+    _description = 'Art Work'
+
+    name = fields.Char(string='Name')
+    description = fields.Text(string='Description', default='Art Work History')
+
+    height = fields.Float(string='Height', required=True, default=1.0)
+    width = fields.Float(string='Width', required=True, default=1.0)
+    
+    finished_date = fields.Date(string='Finished Date', default=date.today())
